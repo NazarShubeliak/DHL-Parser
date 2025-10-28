d3428bd5d345adce2957712fd36e86546d8b79d7
Nazar <test@gmail.com>
2025-10-28 11:02:35 +0200

    feat: added parser module with pipeline.py and root-level run.py entry point

---
+++ parser/pipeline.py
@@ -0,0 +1,9 @@
+def run_pipeline() -> None:
+    """"""
+    # Step 1: clear directory
+
+    # Step 2: run parsing and download all file
+
+    # Step 3: parsing all file and get data
+
+    # Step 4: add data to Google Sheet
---
+++ run.py
@@ -0,0 +1,4 @@
+from parser import run_pipeline
+
+if __name__ == "__main__":
+    run_pipeline()
