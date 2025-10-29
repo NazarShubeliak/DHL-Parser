ce69b69bde58b6251dd5ae98df7f34692c766e1a
Nazar <test@gmail.com>
2025-10-29 10:04:57 +0200

    feat(tools): added unzip function to extract and clean up download archive

--- parser/pipeline.py
+++ parser/pipeline.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from parser.tools import clean_folder
+from parser.tools import clean_folder, unzip_archive
 from config import DATA_DIR, logger, URL
 from parser.scraper.scraper import run_scraper
 
@@ -9,10 +9,15 @@ def run_pipeline(start_date: str, end_date: str, download_dir: Path, type: str) 
     clean_folder(DATA_DIR)
     logger.info(f"Delete all from dir: {DATA_DIR}")
 
-    # Step 2: run parsing and download all file
+    # Step 2: run scraper and download all file
     url = f"{URL}?billing-from={start_date}&billing-until={end_date}"
     run_scraper(type, url, download_dir)
+    logger.info(f"Run scraper for type:{type} date:{start_date} to {end_date}")
 
-    # Step 3: parsing all file and get data
+    # Step 3: unzip archive
+    unzip_archive(download_dir)
+    logger.info("Unzip archive")
 
-    # Step 4: add data to Google Sheet
+    # Step 4: parsing all file and get data
+
+    # Step 5: add data to Google Sheet
--- parser/tools.py
+++ parser/tools.py
@@ -1,10 +1,11 @@
 """
 Utility functions for Klarna parser.
 
-Includes helpers for file system cleanup and Chrome options.
+Includes helpers for file system cleanup, Chrome options and uzip arhive.
 """
 
 import shutil
+import zipfile
 from pathlib import Path
 from config import logger, DEBUG_MODE
 from selenium.webdriver.chrome.options import Options
@@ -64,3 +65,30 @@ def get_chrome_options(download_dir: Path) -> Options:
         options.add_argument("--start-maximized")
 
     return options
+
+
+def unzip_archive(folder_path: Path) -> None:
+    """
+    Unzips all ZIP files in the given folder and deletes them after successful extraction.
+
+    Args:
+        folder_path (Path): Directory containing ZIP files to extract.
+    """
+    folder_path = folder_path.resolve()
+
+    if not folder_path.exists():
+        logger.error(f"Directory not found: {folder_path}")
+        raise FileNotFoundError(f"Directory not found: {folder_path}")
+
+    for zip_file in folder_path.iterdir():
+        if zip_file.is_file() and zip_file.suffix.lower() == ".zip":
+            try:
+                with zipfile.ZipFile(zip_file, "r") as zip_ref:
+                    zip_ref.extractall(folder_path)
+                logger.info(f"Unzipped: {zip_file.name}")
+
+                zip_file.unlink()
+                logger.info(f"Deleted archive: {zip_file.name}")
+            except zipfile.BadZipFile:
+                logger.error(f"Invalid ZIP file: {zip_file.name}")
+                
